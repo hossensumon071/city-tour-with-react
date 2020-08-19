0dfa99d5d5182257c2483cded8ac3a9f19bb248c
hossensumon071 <hossensumon071@gmail.com>
2020-08-19 16:32:35 +0600

    work with Tour component Logic

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Tour.scss';
-export default function Tour() {
- return (
-  <h1>
-   hello from tour
-  </h1>
- )
+
+export default class Tour extends Component {
+ render() {
+  const {id, city, name,img ,info} = this.props.tour;
+  const { removeTour } = this.props;
+  return (
+   <article className="tour">
+    <div className="img-container">
+     <img src={img} alt="image of the tour "/>
+     <span className="close-btn">
+       <i className="fa fa-window-close"></i>
+     </span>
+    </div>
+    <div className="tour-info">
+     <h3>{city}</h3>
+     <h4>{name}</h4>
+     <h5>
+      info{""}
+      <span>
+       <i className="fas fa-caret-square-down"/>
+      </span>
+     </h5>
+  <p>{info}</p>
+    </div>
+   </article>
+  )
+ }
 }
+
+
